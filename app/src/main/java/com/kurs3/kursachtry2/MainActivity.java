d786d8b86d1a306f5aed0d5cbdfa5a28d208bd7a
murmax <maxim_mour@mail.ru>
2018-02-17 13:44:55 +0300

    Initial commit

---
+++ app/src/main/java/com/kurs3/kursachtry2/MainActivity.java
@@ -0,0 +1,13 @@
+package com.kurs3.kursachtry2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
